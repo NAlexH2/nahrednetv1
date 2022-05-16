ad308c6a83f0f95cb89f7fae735f4cc5bbd5bebf
LazG7 <nharris@pdx.edu>
2022-05-15 20:25:17 -0700

    Custome class for basic general text.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ function App() {
       <header className="App-header">
         <img className="my-picture" src={alexharrisphoto}/>
         <p>Reach me at <a className="Any-link" href="mailto:alex@nah-red.net">alex@nah-red.net</a></p>
+        <div className="General-text">
+        I'm a Senior at Portland State University chasing a Computer Science degree.<br></br>
+        If you're here, you've either found me by searching the web, or I've provided the link directly!
+        </div>
+        <h3>Welcome!</h3>
       </header>
     </div>
   );
