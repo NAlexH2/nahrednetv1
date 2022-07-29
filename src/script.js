e43955e75410c58b6189326b0e4e4becf64a66b9
Alex Harris <nharris356087@gmail.com>
2022-07-28 20:03:34 -0700

    Major progress made.

---
+++ src/script.js
@@ -0,0 +1,40 @@
+const greeting = document.querySelector("#greeting");
+const weatherconditions = document.querySelector("#currentcons");
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=45.511383&lon=-122.685347&appid=45acaf3707d2524d96b4d869a0e2425d";
+
+const GiveThemAHello = () => {
+  let element = document.createElement("div");
+  let currentTime = new Date(Date.now());
+  let tod = currentTime.getHours();
+  if (tod < 12) element = "Good morning!";
+  else if (tod < 16) element = "Good afternoon!";
+  else element.textContent = "Good evening!";
+  element.style.textDecoration = "underline rgba(255, 196, 0, 0.87)";
+  greeting.append(element);
+  return;
+};
+
+const HeresTheWeatherToo = (url) => {
+  let element = document.createElement("div");
+  $.get({
+    type: "GET",
+    url: url,
+    success: (temps) => {
+      element.textContent =
+        "The weather in the Portland park blocks is currently: " +
+        parseInt((temps.main.temp - 273.15) * 1.8 + 32) +
+        "°F - " +
+        temps.weather[0].description +
+        ".";
+    },
+    error: (err) => {
+      console.log(err);
+    },
+    finally: () => {},
+  });
+  weatherconditions.append(element);
+};
+
+GiveThemAHello();
+HeresTheWeatherToo(url);
