c9c997ab73c24de962ad62443b62de60bebdfac3
Alex Harris <nharris356087@gmail.com>
2022-07-30 14:23:38 -0700

    bio finished

--- src/header.js
+++ src/header.js
@@ -3,6 +3,7 @@ document.write(
     <meta charset="UTF-8" />\
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />\
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />\
+    <title>nah-red.net</title>\
     <link rel="preconnect" href="https://fonts.googleapis.com" />\
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />\
     <link rel="preconnect" href="https://fonts.googleapis.com" />\
@@ -13,7 +14,6 @@ document.write(
       href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Square+Peg&display=swap"\
       rel="stylesheet"\
     />\
-    <title>nah-red.net</title>\
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>\
     <link\
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"\
