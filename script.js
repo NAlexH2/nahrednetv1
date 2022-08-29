003e25db785ea7255a9e0d02f187c20c58524075
Alex Harris <nharris356087@gmail.com>
2022-08-29 13:44:06 -0700

    Removed current weather.

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 const greeting = document.querySelector("#greeting");
-const weatherconditions = document.querySelector("#currentcons");
-const url =
-  "https://api.openweathermap.org/data/2.5/weather?lat=45.511383&lon=-122.685347&appid=45acaf3707d2524d96b4d869a0e2425d";
 
 const GiveThemAHello = () => {
   let element = document.createElement("div");
@@ -14,22 +11,4 @@ const GiveThemAHello = () => {
   return;
 };
 
-const HeresTheWeatherToo = (url) => {
-  $.get({
-    type: "GET",
-    url: url,
-    success: (temps) => {
-      currentTemps = parseInt((temps.main.temp - 273.15) * 1.8 + 32);
-      currentClouds = temps.weather[0].description;
-      $("#currentTemp").text(currentTemps);
-      $("#currentClouds").text(currentClouds + ".");
-    },
-    error: (err) => {
-      console.log(err);
-    },
-    finally: () => {},
-  });
-};
-
 GiveThemAHello();
-HeresTheWeatherToo(url);
